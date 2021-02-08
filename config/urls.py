28a70ad649078c322cbb6400dd6a9c472fc9d02f
northfieldzz <47939848+northfieldzz@users.noreply.github.com>
2021-02-09 02:57:03 +0900

    Update: Swaggerコメント

--- config/urls.py
+++ config/urls.py
@@ -22,12 +22,12 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="Gorilla Achieve API",
         default_version='v1',
-        description="Test description",
+        description="日常実績ゴリラのためのAPI",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
+        contact=openapi.Contact(email="northfield.t@outlook.com"),
+        license=openapi.License(name="MIT License"),
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
